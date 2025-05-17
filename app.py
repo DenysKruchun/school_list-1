b48ebd351e816f543efab181daaa568bd546dcf9
Volodymyr <webteach.ua@gmail.com>
2025-05-17 12:53:00 +0300

    .

--- app.py
+++ app.py
@@ -11,11 +11,15 @@ app.config["SECRET_KEY"] = "reve ta stohne dnipr"
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-from models import Student
+from models import Student,Teacher,Subject
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
-    return render_template("index.html")  # html-сторінка, що повертається у браузер
+    teachers = Teacher.query.filter_by(full_name = "Ann").first()
+    subject = Subject(name = "PE")
+    db.session.add(subject)
+    db.session.commit()
+    return render_template("index.html",teachers = teachers)  # html-сторінка, що повертається у браузер
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -11,4 +11,27 @@ class Student(db.Model):
     def __repr__(self):
         return f'Student {self.full_name}'
 
-# створити класи Teacher, Subject, Grade, Group
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(70), nullable=False)
+    birth_day = db.Column(db.Date,nullable = False)
+    email_adress = db.Column(db.String(100), nullable=False)
+    phone_num = db.Column(db.String(20), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"))
+
+    def __repr__(self):
+        return f'Teacher {self.full_name}'
+
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50),nullable=False)
+    teachers = db.relationship("Teacher", backref="subject")
+
+    def __repr__(self):
+        return f'Subject {self.name}'
+
+
+# створити класи ClassGroup, Lesson(урок) Grade (оцінка), 
+ 
